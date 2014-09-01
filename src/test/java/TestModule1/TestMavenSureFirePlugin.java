39009ba4adfd3bc82c1144930a788248a25985e0
AlwaysTerry <admin123>
2014-09-01 10:57:56 +0800

    qwe

--- src/test/java/TestModule1/TestMavenSureFirePlugin.java
+++ src/test/java/TestModule1/TestMavenSureFirePlugin.java
@@ -20,7 +20,7 @@ public class TestMavenSureFirePlugin {
 	
 	@BeforeClass
 	public void beforeClass(){
-		Browsers browser = new Browsers(BrowsersType.chrome);
+		Browsers browser = new Browsers(BrowsersType.firefox);
 		driver = browser.driver;
 	}
 
