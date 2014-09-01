09da3e8ec179d9394ac2024529201469e2d60e36
AlwaysTerry <admin123>
2014-09-01 11:01:11 +0800

    qa

--- src/main/java/utils/Browsers.java
+++ src/main/java/utils/Browsers.java
@@ -18,7 +18,7 @@ public class Browsers {
 	public WebDriver driver = null;
 	private FirefoxProfile firefoxprofile = null;
 	private static DesiredCapabilities caps = null;
-	private String projectpath ="G:\\SeleniumTraining1\\FireflyAutomation\\trunk";
+	private String projectpath =System.getProperty("user.dir");
 
 	
 	public Browsers(BrowsersType browserstype){
